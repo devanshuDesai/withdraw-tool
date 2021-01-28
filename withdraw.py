8148a0d6c1e57dcda17af871a8562a76b17fb91e
Devanshu Desai <dbdesai@ucsd.edu>
2021-01-27 18:57:56 -0800

    added sorting for missing csv

--- withdraw.py
+++ withdraw.py
@@ -22,8 +22,8 @@ late_df.loc[:, late_cols] = late_df.loc[:, late_cols].apply(
     lambda col: col.dt.total_seconds() > 0, axis=1)
 late_df['slip_days'] = late_df.loc[:, late_cols].apply(
     lambda row: row.sum(), axis=1)
-late_df = late_df.sort_values('slip_days', ascending=False)
 late_df = late_df.loc[:, detail_cols+['slip_days']]
+late_df = late_df.sort_values('slip_days', ascending=False)
 late_df.to_csv(f'slip_days_{NUM_GRADED}.csv', index=False)
 
 # 2. Consecutive assignments not turned in
@@ -43,4 +43,6 @@ missing_df = df.loc[:, detail_cols+score_cols]
 missing_df.loc[:, score_cols] = missing_df.loc[:, score_cols].isnull()
 missing_df['missed_assignments'] = missing_df.loc[:,
                                             score_cols].apply(get_consecutive, axis=1)
-missing_df.csv(f'missing_{NUM_GRADED}.csv')
+missing_df = missing_df.loc[:,detail_cols+['missed_assignments']]
+missing_df = missing_df.sort_values('missed_assignments', ascending=False)
+missing_df.to_csv(f'missing_{NUM_GRADED}.csv', index=False)
