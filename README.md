a21edf58a7908b4eb28993922a9a8c71d570517d
Devanshu Desai <dbdesai@ucsd.edu>
2021-01-28 01:31:09 -0800

    added CLI functionality

--- README.md
+++ README.md
@@ -1,14 +1,28 @@
 # Withdrawal Tool
 ### A tool made for gathering DSC 40B specific email lists automatically
 
+## Examples
 To run,
-`python run.py`
+```python run.py --count 2```
+
+Here `--count` is a mandatory argument for now to specify how many homeworks
+have been graded so far. This is to not confuse the script with homeworks
+that have been submitted but not graded so it becomes difficult to tell if the student submitted the homework or not. I am trying to get the code to infer this automatically in a future build.
 
 This command generates two separate `csv` files:
 1. `slip_days_{x}` includes the number of slip days each student has used.
 2. `missing_{x}` includes the number of consecutive assignments not turned in by the student.
+where {x} is 2 in this example.
+
+You can also specify custom file paths to the gradebook.
+```python run.py --path ./gradebook.csv --count 2```
+
+You can also add `--email` to only get the emails of the students who have not submitted at least one assignment or have used at least one slip day. I will be trying to eventually make this send emails automatically.
+```python run.py --count 2 --email```
 
 ## TODO:
 - Automatically detect how many assignments are graded.
-- Implement an `argparse` API.
+- ~Implement an `argparse` API.~
+- Set up an email server to work with the script.
+- Add more structure to the codebase
 
--- run.py
+++ run.py
@@ -3,17 +3,24 @@ import argparse
 import pandas as pd
 
 GRADEBOOK_PATH = '~/Downloads/DSC_40B_Winter_2021_grades.csv'
-NUM_GRADED = 2  # TODO: Find a way to automate this
-
-df = pd.read_csv(GRADEBOOK_PATH)
 parser = argparse.ArgumentParser()
+parser.add_argument('--path', metavar='-p',
+                    default=GRADEBOOK_PATH, help='Path to Gradebook')
+# TODO: Find a way to automate this
+parser.add_argument('--count', metavar='-c', type=int,
+                    help='Number of HWs graded so far')
+# TODO: Add functionality to automatically send out emails here
+parser.add_argument('--email', action='store_true', help='Only output the emails of the students with non-zero missed assignments (default: output all emails)')
+args = parser.parse_args()
+
+df = pd.read_csv(args.path)
 
 # Selecting correct columns
 detail_cols = ['Name', 'Email']
 late_cols = list(filter(lambda x: re.match(r'^[\w\d\s]+ - Lateness.*', x),
-                        df.columns))[:NUM_GRADED]
+                        df.columns))[:args.count]
 score_cols = list(filter(lambda x: re.match(r'^[\w\d\s]+\d$', x),
-                         df.columns))[:NUM_GRADED]
+                         df.columns))[:args.count]
 # Note that we're filtering the programming assignments
 
 # Converting columns to timedelta objects
@@ -28,7 +35,11 @@ late_df['slip_days'] = late_df.loc[:, late_cols].apply(
     lambda row: row.sum(), axis=1)
 late_df = late_df.loc[:, detail_cols+['slip_days']]
 late_df = late_df.sort_values('slip_days', ascending=False)
-late_df.to_csv(f'slip_days_{NUM_GRADED}.csv', index=False)
+
+# Saving the results
+if args.email:
+    late_df = late_df[late_df.slip_days > 0]
+late_df.to_csv(f'slip_days_{args.count}.csv', index=False)
 
 # 2. Consecutive assignments not turned in
 
@@ -46,7 +57,11 @@ def get_consecutive(arr):
 missing_df = df.loc[:, detail_cols+score_cols]
 missing_df.loc[:, score_cols] = missing_df.loc[:, score_cols].isnull()
 missing_df['missed_assignments'] = missing_df.loc[:,
-                                            score_cols].apply(get_consecutive, axis=1)
-missing_df = missing_df.loc[:,detail_cols+['missed_assignments']]
+                                                  score_cols].apply(get_consecutive, axis=1)
+missing_df = missing_df.loc[:, detail_cols+['missed_assignments']]
 missing_df = missing_df.sort_values('missed_assignments', ascending=False)
-missing_df.to_csv(f'missing_{NUM_GRADED}.csv', index=False)
+
+# Saving the results
+if args.email:
+    missing_df = missing_df[missing_df.missed_assignments > 0]
+missing_df.to_csv(f'missing_{args.count}.csv', index=False)
